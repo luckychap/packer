4b07d4b731ef60214487d91d72e63f7e797c910b
Lucky <lakatos.martin@gmail.com>
2018-10-23 08:38:44 +0200

    VirtualBox bash make sure all necessary packages are installed

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yum install -y gcc cpp make perl bzip2 kernel-devel kernel-headers
+
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
